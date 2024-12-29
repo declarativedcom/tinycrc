a1692c956cf354ff33b035109952af08b79c7217
Henrique Moreira <h@serrasqueiro.com>
2024-12-29 18:59:24 +0000

    How-to(s)

--- main.c
+++ main.c
@@ -9,13 +9,14 @@
 #include <string.h>
 #include "crc.h"
 
+typedef unsigned char uint8;
 typedef unsigned int uint32;
 
 int calc_crc32(FILE* fin, unsigned* res)
 {
  crc_t crc = crc_init();
  size_t i = 0;
- unsigned char u;
+ uint8 u;
  while (1) {
      if (fscanf(fin, "%c", &u) == -1) break;
      crc = crc_update(crc, &u, 1);
@@ -26,10 +27,21 @@ int calc_crc32(FILE* fin, unsigned* res)
  return 0;
 }
 
+uint32 byte_crc32(const char* str)
+{
+ crc_t crc = crc_init();
+ if (str == NULL) {
+     return 0;
+ }
+ crc = crc_update(crc, str, strlen(str));
+ return crc_finalize(crc);
+}
+
 int main(int argc, char* argv[])
 {
- int i, code = 0;
+ int i, istart = 1, code = 0;
  uint32 u_crc = 0;
+ const char* str;
  FILE* fin;
  argc--;
  if (argc <= 0) {
@@ -37,7 +49,17 @@ int main(int argc, char* argv[])
      printf("0x%08x\n", u_crc);
      return code;
  }
- for (i=1; i<=argc; i++) {
+ if (strcmp(argv[istart], "--") == 0) {
+     /* Assume it's a string next */
+     istart++;
+     for (i=istart; i<=argc + (istart==argc+1); i++) {
+	 str = argv[i] ? argv[i] : "123456789";
+	 u_crc = byte_crc32(str);
+	 printf("0x%08x\n", u_crc);
+     }
+     return 0;
+ }
+ for (i=istart; i<=argc; i++) {
      const char* fname = argv[i];
      u_crc = 0;
      fin = fopen(fname, "rb");
--- tiny-crc-32.md
+++ tiny-crc-32.md
@@ -2,3 +2,16 @@
 Compile:
 1. `gcc -W -Wall -std=c99 crc.c main.c -s -o crc32`
 1. `sudo mv crc32 /usr/local/bin`
+1. `crc32 --` ---> shows `0xcbf43926`, which is the default string '123456789'
+
+## Making use of simple _crc32_
+This tiny `crc32` (CRC-32 calculator, polynomial 0x4c11db7) has _main.c_ with a few options:
+1. No arguments: uses stdin
+1. Argument starting with `--`
+   + Calculates CRC-32 of the several words entered in the arguments
+1. Else:
+   + It shows the files in the arguments.
+
+## Reference
+1. Other kinds of CRC-32 listed:
+   * [(here)](https://crccalc.com/?crc=123456789&method=CRC-32/ISO-HDLC&datatype=ascii&outtype=hex) CRC calc.com, example for string `123456789`
